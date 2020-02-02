5f8a46ac2d74f030247a7aa964ee91a03b1f5883
djjproject <djj9404@gmail.com>
2020-02-02 17:10:37 +0900

    add openssh server install

--- buildroot.sh
+++ buildroot.sh
@@ -1,30 +1,73 @@
 #!/bin/bash
 
-echo "### Android Over Linux buildroot script"
-echo "### You should run this script debian/ubuntu based OS"
+# configuration
+ROOTFS=rootfs
 
-echo "### install dependent packages..."
-apt update && apt install -y debootstrap qemu-user-static
+# echo color output
+RED='\033[0;31m'
+NC='\033[0m'
+GREEN='\033[0;32m'
 
-echo "### make debian10 buster rootfs..."
-mkdir rootfs
-debootstrap --arch armhf --foreign buster rootfs/ http://httpredir.debian.org/debian
+function logn {
+	echo -e "${RED}[BUILDROOT]${GREEN} $1 ${NC}"
+}
 
-echo "### add buster repository urls..."
-cat <<'EOF' > rootfs/etc/apt/sources.list
+function loge {
+	echo -e "${RED}[BUILDROOT]${RED} $1 ${NC}"
+}
+
+function chrun {
+	chroot $ROOTFS/ $1
+}
+
+logn "### Android Over Linux buildroot script"
+logn "### You should run this script debian/ubuntu based OS"
+
+logn "### cleanup buildroot..."
+rm -rf $ROOTFS
+
+logn "### install dependent packages..."
+apt update && apt install -y debootstrap binfmt-support qemu-user-static
+
+logn "### make debian10 buster $ROOTFS..."
+mkdir $ROOTFS
+logn "### rootfs directory `readlink -e "$ROOTFS"`..."
+debootstrap --arch armhf --foreign buster $ROOTFS/ http://ftp.lanet.kr/debian
+
+logn "### run debian10 buster $ROOTFS second stage..."
+cp /usr/bin/qemu-arm-static $ROOTFS/usr/bin
+chrun "/bin/bash /debootstrap/debootstrap --second-stage"
+
+logn "### add buster repository urls..."
+cat <<'EOF' > $ROOTFS/etc/apt/sources.list
 # debian stretch repo
-deb http://httpredir.debian.org/debian/ buster main contrib non-free
-deb-src http://httpredir.debian.org/debian/ buster main contrib non-free
+deb http://ftp.lanet.kr/debian/ buster main contrib non-free
+deb-src http://ftp.lanet.kr/debian/ buster main contrib non-free
 
 # debian stretch-backports repo
-deb http://httpredir.debian.org/debian buster-backports main contrib non-free
-deb-src http://httpredir.debian.org/debian buster-backports main contrib non-free
+deb http://ftp.lanet.kr/debian buster-backports main contrib non-free
+deb-src http://ftp.lanet.kr/debian buster-backports main contrib non-free
 
 # debian stretch-updates repo
-deb http://httpredir.debian.org/debian buster-updates main contrib non-free
-deb-src http://httpredir.debian.org/debian buster-updates main contrib non-free
+deb http://ftp.lanet.kr/debian buster-updates main contrib non-free
+deb-src http://ftp.lanet.kr/debian buster-updates main contrib non-free
 EOF
 
-echo "### install openssh server..."
-chroot rootfs/ /bin/bash apt update
-chroot rootfs/ /bin/bash apt install openssh-server -y
+function chrun {
+	chroot $ROOTFS/ $1
+}
+
+logn "### set root passwd..."
+echo -e "androidoverlinux\nandroidoverlinux\n" | chrun "/bin/passwd root"
+
+logn "### set hostname..."
+echo "AOL-Debian" > $ROOTFS/etc/hostname
+
+logn "### install openssh server..."
+chrun "/bin/apt update"
+chrun "/usr/bin/hostname $(cat /etc/hostname)"
+chrun "/bin/apt install openssh-server -y"
+hostname $(cat /etc/hostname)
+
+
+
