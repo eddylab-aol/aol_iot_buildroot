7b9412a0b6157953a3b05541a87a3ee559b3638d
root <root@debian10.linuxvmimages.local>
2020-11-19 23:26:14 +0900

    [buildroot] fix rm rc6 services command

--- buildroot.sh
+++ buildroot.sh
@@ -135,19 +135,19 @@ sed -i -e "s/passwd:         files systemd/passwd:         files/g" $ROOTFS/etc/
 sed -i -e "s/group:          files systemd/group:          files/g" $ROOTFS/etc/nsswitch.conf
 
 logn "### fix rc3.d / rc6.d..."
-rm /etc/rc3.d/*bootlogs
-rm /etc/rc3.d/*rmnologin
+rm $ROOTFS/etc/rc3.d/*bootlogs
+rm $ROOTFS/etc/rc3.d/*rmnologin
 
-rm /etc/rc6.d/*brightness
-rm /etc/rc6.d/*udev
-rm /etc/rc6.d/*urandom
-rm /etc/rc6.d/*sendsigs
-rm /etc/rc6.d/*hwclock.sh
-rm /etc/rc6.d/*umountnfs.sh
-rm /etc/rc6.d/*networking
-rm /etc/rc6.d/*umountfs
-rm /etc/rc6.d/*umountroot
-rm /etc/rc6.d/*reboot
+rm $ROOTFS/etc/rc6.d/*brightness
+rm $ROOTFS/etc/rc6.d/*udev
+rm $ROOTFS/etc/rc6.d/*urandom
+rm $ROOTFS/etc/rc6.d/*sendsigs
+rm $ROOTFS/etc/rc6.d/*hwclock.sh
+rm $ROOTFS/etc/rc6.d/*umountnfs.sh
+rm $ROOTFS/etc/rc6.d/*networking
+rm $ROOTFS/etc/rc6.d/*umountfs
+rm $ROOTFS/etc/rc6.d/*umountroot
+rm $ROOTFS/etc/rc6.d/*reboot
 
 logn "### fix rc PATH..."
 sed -i -e 's,^PATH=,PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin,g' $ROOTFS/lib/init/rc
@@ -179,7 +179,7 @@ echo "ro.build.version.linux=$(date "+%Y%m%d").120000" > $ROOTFS/linux.prop
 
 ######### buildroot independent parts #########
 logn "### run buildroot independent parts..."
-source $SERVICE_DIR/buildroot-install.sh
+#source $SERVICE_DIR/buildroot-install.sh
 
 logn "### clean apt cache..."
 chrun "apt-get clean"
